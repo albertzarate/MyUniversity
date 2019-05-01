c3e09b333a1d77c39d7600267ec279cb158a2309
albertzarate <thealbertzarate@gmail.com>
2019-05-01 04:12:24 +0000

    this auth stuff works

---
+++ venv/lib/python3.6/site-packages/authlib/client/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+from .oauth1_session import OAuth1Session, OAuth1Auth
+from .oauth2_session import OAuth2Session, OAuth2Auth
+from .assertion_session import AssertionSession
+from .client import OAuthClient
+from .errors import *
---
+++ venv/lib/python3.6/site-packages/authlib/jose/jwk.py
@@ -0,0 +1,29 @@
+import json
+from authlib.common.encoding import text_types
+from .rfc7517 import JWK
+from .rfc7518 import JWK_ALGORITHMS
+
+jwk = JWK(algorithms=JWK_ALGORITHMS)
+
+
+def _load_jwk(key, header):
+    if not key and 'jwk' in header:
+        key = header['jwk']
+    if isinstance(key, (tuple, list, dict)):
+        return jwk.loads(key, header.get('kid'))
+    if isinstance(key, text_types) and \
+            key.startswith('{') and key.endswith('}'):
+        return jwk.loads(json.loads(key), header.get('kid'))
+    return key
+
+
+def load_key(key, header, payload):
+    if callable(key):
+        key = key(header, payload)
+    return _load_jwk(key, header)
+
+
+def create_key_func(key):
+    def key_func(header, payload):
+        return load_key(key, header, payload)
+    return key_func
